85982d683019d13ee1ca0ae563ea19cb9cf4c606
vishakagopalan <vishakadatta.sreenivasagopalan@nih.gov>
2026-07-29 16:14:04 -0400

    fix: add imports.R, Python setup in pkgdown workflow

---
+++ imports.R
@@ -0,0 +1,11 @@
+#' @importFrom methods new
+#' @importFrom stats complete.cases dist median
+#' @importFrom utils head
+
+NULL
+
+utils::globalVariables(c(
+  "CC", "PC", "bin", "cell_id", "feature", "feature_name",
+  "feature_type", "iteration", "loading", "loading_mag",
+  "n", "numpy_idx", "sd_val", "x", "y"
+))
